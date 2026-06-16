c2224d07ed88478d66705d601ec4e0eda2b22691
Arghya Ojha <arghya4unofficial@gmail.com>
2026-06-16 23:53:15 +0530

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,46 @@
+//me @lazyflash99
+
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+typedef long long ll;
+typedef __gnu_pbds::tree<ll, __gnu_pbds::null_type, less<ll>, __gnu_pbds::rb_tree_tag, __gnu_pbds::tree_order_statistics_node_update> pbds; //order_of_key //find_by_order
+#define nline '\n'
+#define all(v) v.begin(),v.end()
+#define srt(v) sort(v.begin(),v.end())
+#define minm(v) *min_element(v.begin(),v.end())
+#define maxm(v) *max_element(v.begin(),v.end())
+#define add(v) accumulate(v.begin(),v.end(),0LL)
+#define ff first
+#define ss second
+#define pb push_back
+const unsigned int M = 1000000007;
+
+
+/*
+Thinking...
+
+*/
+
+
+void solve(){
+    
+}
+
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int t;
+    cin>>t;
+    while (t--){
+        solve();
+    }
+}
+
+//Bingo!!
+//shouganai :)
